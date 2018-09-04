b415e6852c3a134ef10ab3958d3510fe54ad0716
Anders Dahlen <anders.dahlen@knowit.no>
2018-09-04 15:15:34 +0200

    Added datasource for AMI

--- cloud/environment/poc/main.tf
+++ cloud/environment/poc/main.tf
@@ -1,10 +1,26 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+variable "account_id" {
+  description = "Account id for the owner of the image"
+}
+
+data "aws_ami" "influx_and_grafana_image" {
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["influx-graphana*"]
+  }
+
+  owners = ["${var.account_id}"]
+}
+
 module "infrastructure" {
   source = "../../infrastructure/aws"
   environment = "poc"
   project     = "sense"
 
-  influxdb_ami = "ami-0c39bb7d1fe8951e7"
+  influxdb_ami = "${data.aws_ami.influx_and_grafana_image.id}"
 }
