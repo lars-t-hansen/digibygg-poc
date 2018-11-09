7ff4ee9c6235d96244666519491c859572ac4f2a
petterlovereide <petter.lovereide@gmail.com>
2018-11-09 12:11:18 +0100

    added new config setup. rewrite of README.

--- esp32_bme280/src/main.c
+++ esp32_bme280/src/main.c
@@ -3,32 +3,41 @@
 #include "mgos_bme280.h"
 
 #define MINUTE 60000
+#define PUBLISH_INTERVAL 5000
 
 static void publish(const char *fmt, ...) {
     char message[200];
     struct json_out json_message = JSON_OUT_BUF(message, sizeof(message));
+
     va_list ap;
     va_start(ap, fmt);
     int n = json_vprintf(&json_message, fmt, ap);
     va_end(ap);
+
     mgos_mqtt_pub(mgos_sys_config_get_mqtt_publish(), message, n, 1, 0);
 }
 
 static void publish_timer_callback(void *sensor_arg) {
     struct mgos_bme280* sensor = (struct mgos_bme280*) sensor_arg;
-    struct mgos_bme280_data data = {.temp=0.0, .press=0.0, .humid=0.0};
+    struct mgos_bme280_data data = {
+        .temp=0.0,
+        .press=0.0,
+        .humid=0.0
+    };
     mgos_bme280_read(sensor, &data);
-    LOG(LL_INFO, ("Temperature: %f, Pressure: %f, Humidity: %f", data.temp, data.press, data.humid));
+    LOG(LL_INFO, ("Location: %s, Temperature: %f, Pressure: %f, Humidity: %f", mgos_sys_config_get_location_name(), data.temp, data.press, data.humid));
     time_t t = time(0);
     struct tm* timeinfo = localtime(&t);
     char timestamp[24];
     strftime(timestamp, sizeof (timestamp), "%FT%TZ", timeinfo);
-    publish("{measurement: %Q, tags: {rom: %Q}, time: %Q, fields: {Temperature: %f, Pressure: %f, Humidity: %f}}", "bme280", "Kontor: Christer", timestamp, data.temp, data.press, data.humid);
+    
+    publish("{measurement: %Q, tags: {rom: %Q}, time: %Q, fields: {Temperature: %f, Pressure: %f, Humidity: %f}}", "bme280", mgos_sys_config_get_location_name(), timestamp, data.temp, data.press, data.humid);
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-    struct mgos_bme280* sensor = mgos_bme280_i2c_create(119);
-    mgos_set_timer(5000, true, publish_timer_callback, sensor);
+    struct mgos_bme280* sensor = mgos_bme280_i2c_create(0x76);
+    mgos_set_timer(PUBLISH_INTERVAL, true, publish_timer_callback, sensor);
+
     return MGOS_APP_INIT_SUCCESS;
 }
 
